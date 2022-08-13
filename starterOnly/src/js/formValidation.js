69874488a737aacb4ef1379f33f14cfed8fafc6b
Thierry-Heinz <studioklatz@gmail.com>
2022-08-13 11:48:24 +0200

    commit before soutenance. Checked visual compliance, added comments

--- starterOnly/src/js/formValidation.js
+++ starterOnly/src/js/formValidation.js
@@ -1,9 +1,11 @@
-//Form validation functionality
+/**
+ * Form validation functionality
+ **/
 
-//DOM elements
+//DOM form element
 const formEl = document.querySelector("form");
 
-//form fields for validation
+//DOM form fields
 const firstNameEl = document.querySelector("#firstName");
 const lastNameEl = document.querySelector("#lastName");
 const emailEl = document.querySelector("#email");
@@ -12,18 +14,16 @@ const birthDateEl = document.querySelector("#birthdate");
 const radioEls = document.getElementsByName("location");
 const userCheckboxEl = document.querySelector("#checkbox1");
 const userPreferenceEl = document.querySelector("#checkbox2");
-<<<<<<< HEAD
 
+// Regex for ulterior validation
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const regexDate = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-=======
->>>>>>> 9a80c2b3ced2983856a7e8f91bc5e520f4b7086c
 
 /**
  * Utility Functions
  **/
 
-//Check if not Empty
+//Check if the input fields is not Empty
 const isRequired = (value) => {
   if (value === "") {
     return false;
@@ -32,22 +32,23 @@ const isRequired = (value) => {
   }
 };
 
+//Trim the value from the input
 const getValTrim = (el) => el.value.trim();
 
-//Check text is minimun length
+//Check if text value is minimun length
 const isMinLength = (length, min) => (length < min ? false : true);
 
-//Check is number
+//Check if value is a number
 const isNumber = (number) =>
   typeof number === "number" && !Number.isNaN(number);
 
-//Check email expression
-const isEmailValid = (email) => regex.test(email);
+//Check if the value is an email expression
+const isEmailValid = (email, regex) => regex.test(email);
 
-//Test the Date
+//Test the Date formatting
 const isDateYmd = (date, regex) => regex.test(date);
 
-//Check if radio is selected
+//Check if a radio field is selected
 const isRadioChecked = (radioEls) => {
   let oneRadioChecked = false;
 
@@ -59,48 +60,40 @@ const isRadioChecked = (radioEls) => {
   return oneRadioChecked;
 };
 
-//Check if checkbox is checked
+//Check if the checkbox is checked
 const isCheckboxChecked = (checkboxEl) => checkboxEl.checked;
 
-// Validation Function
-//Name validation
+/**
+ * Validation Function
+ */
+
+// Name validation
 const validateName = (name, minLength) =>
   isRequired(name) && isMinLength(name.length, minLength);
 
-// Validate Email
-<<<<<<< HEAD
+// Email Validation
 const validateEmail = (email) =>
   isRequired(email) && isEmailValid(email, regexEmail);
-=======
-const validateEmail = (email) => isRequired(email) && isEmailValid(email);
->>>>>>> 9a80c2b3ced2983856a7e8f91bc5e520f4b7086c
 
-//Validate the birthdate
-const validateBD = (date) => {
+// Birthdate Validation
+const validateBD = (date, regex) => {
   const BDdate = new Date(date);
   const today = new Date();
-
-<<<<<<< HEAD
   return (
     isRequired(date) &&
-    isDateYmd(date, regexEmail) &&
+    isDateYmd(date, regex) &&
     (BDdate < today ? true : false)
   );
-=======
-  return isRequired(date) && isDateYmd(date) && (BDdate < today ? true : false);
->>>>>>> 9a80c2b3ced2983856a7e8f91bc5e520f4b7086c
 };
 
-// Validate quantity field
+// Quantity field Validation
 const validateQty = (qty) => isRequired(qty) && isNumber(qty);
 
 /**
- *
  * Visual Function
- *
  */
 
-// Show error messages under input of .formData
+// Show error messages under input of formData class
 const showError = (el, bool, message) => {
   const formData = el.parentElement;
 
@@ -115,10 +108,13 @@ const showError = (el, bool, message) => {
   }
 };
 
-// Show a validation message if all input of the form are true.
+// Show a validation message if all the inputs checked are valid.
 const validationMessage = (bool) => {
   const formDatas = formEl.querySelectorAll(".formData");
   const successHolder = document.querySelector("#successHolder");
+  const successMessageHolder = successHolder.querySelector(
+    ".successHolder__message"
+  );
   if (bool) {
     for (const formData of formDatas) {
       formData.setAttribute("data-error-visible", false);
@@ -126,19 +122,25 @@ const validationMessage = (bool) => {
       formData.querySelector("input").value = "";
     }
     formEl.style.display = "none";
-    successHolder.textContent = "Merci ! Votre réservation a été reçue.";
+    successHolder.style.display = "block";
+    successMessageHolder.textContent = "Merci ! Votre réservation a été reçue.";
   } else {
-    successHolder.textContent = "";
+    successMessageHolder.textContent = "";
   }
 };
 
 /**
  * Form submit listener
  *
+ * - prevent normal behavior
+ * - use utility validation function on selected form input fields
+ * - use visual function to show error (by passing the message)
+ * - Check if validation is successful on each input fields
+ * - Display success message if validation of input fields succeeded
+ *
  **/
 
 formEl.addEventListener("submit", function (e) {
-  // prevent the form from submitting
   e.preventDefault();
 
   let isFormValid = false;
@@ -156,12 +158,12 @@ formEl.addEventListener("submit", function (e) {
   );
   let isEmailValid = showError(
     emailEl,
-    validateEmail(getValTrim(emailEl)),
+    validateEmail(getValTrim(emailEl), regexEmail),
     "Veuillez entrer une adresse email valide."
   );
   let isBDValid = showError(
     birthDateEl,
-    validateBD(getValTrim(birthDateEl)),
+    validateBD(getValTrim(birthDateEl), regexDate),
     "Vous devez entrer votre date de naissance."
   );
   let isQuantityValid = showError(
--- starterOnly/src/js/modal.js
+++ starterOnly/src/js/modal.js
@@ -1,26 +1,30 @@
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-
-const modalOpenBtns = document.querySelectorAll(".modal-btn");
-const modalCloseBtn = document.querySelector(".modal-close");
+const modalOpenBtns = document.querySelectorAll(".modal-launch-func");
+const modalCloseBtns = document.querySelectorAll(".modal-close-func");
 
 // launch modal event on each buttons
 for (const button of modalOpenBtns) {
   button.addEventListener("click", launchModal);
 }
 
-// launch modal form
+// close modal event on each buttons
+for (const button of modalCloseBtns) {
+  button.addEventListener("click", closeModal);
+}
+
+// Close listeners for the background
+modalbg.addEventListener("click", closeModal);
+
+// Launch modal function
 function launchModal() {
   modalbg.style.display = "block";
-  modalbg.querySelector("#successHolder").textContent = "";
+  modalbg.querySelector("#successHolder").style.display = "none";
+  modalbg.querySelector(".successHolder__message").textContent = "";
   modalbg.querySelector("form").style.display = "block";
 }
 
-// Close modal listener for both background and the close button
-modalbg.addEventListener("click", closeModal);
-modalCloseBtn.addEventListener("click", closeModal);
-
-// Close Modal function
+// Close Modal function (if exactly clicked on the el)
 function closeModal(event) {
   if (event.target === this) {
     modalbg.style.display = "none";
